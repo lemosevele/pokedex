e3b0b130e1d1792b411c839ed93c05b2e68da41b
Evele <lemosevele@gmail.com>
2020-07-11 16:34:01 -0300

    segundo card

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 
     const search_id = document.querySelector("#id_search");
     const search_button = document.querySelector("#search");
+
     const name = document.querySelector("#name");
     const number = document.querySelector("#number");
     const img = document.querySelector("#img");
+
+    const type = document.querySelector("#type");
+    const skills = document.querySelector("#skills");
+    const weight = document.querySelector("#weight");
+    const height = document.querySelector("#height");
+
 
     const apiUrl = 'https://pokeapi.co/api/v2/pokemon';
 
@@ -38,31 +45,26 @@
     async function getPokemon(id){
         const response = await fetch(`${apiUrl}/${id}`);
         const data = await response.json();
+        const maxCaracteres = 852;
 
         name.textContent = data.name;
         number.textContent = "#"+data.id;
         img.src = data.sprites.front_default;
 
-        console.log(data.types.map(item => ' ' + item.type.name).toString());
+        type.textContent = data.types.map(item => ' ' + item.type.name).toString();
+        weight.textContent = data.weight/10+"kg";
+        height.textContent = data.height/10+"m";
+
+        sk = data.moves.map(item => ' ' + item.move.name).toString();
+        if (sk.length <= maxCaracteres) {
+            skills.textContent = sk;
+        } else{
+            var subSkill = sk.slice(0, maxCaracteres);
+            subSkill = subSkill + "...";
+            skills.textContent = subSkill;
+        }
 
         return data;
     }
 
 })();
-
-
-
-
-
-// async function getPokemon(url) {
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   return data;
-// }
-
-// getPokemon(apiUrl)
-//   .then(data => {
-//     const pokemonName = data.name;
-//     name.innerHTML = pokemonName;
-//   })
-//   .catch(error => console.log(`Error: ${error.message}`))
